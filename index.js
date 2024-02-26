d27fdebec3429bfce19f5bb8315d0f9efcafcedd
Devilisded <142964931+Devilisded@users.noreply.github.com>
2024-02-26 16:09:46 +0530

    update cors

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ import authAccount from "./routes/account.js";
 import authContact from "./routes/contact.js";
 import authAdmin from "./routes/admin.js";
 const app = express();
+app.use(express.static("./public"));
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+var whitelist = ["https://www.propertyease.in", "https://propertyease.in"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authLogin);
 app.use("/api/act", authAccount);
 app.use("/api/pro", authProperty);
 app.use("/api/contact", authContact);
 app.use("/api/admin", authAdmin);
-app.use(express.static("./public"));
 app.listen(8010, () => {
   console.log("App is running ");
 });
