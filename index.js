e4642998dc3967b0ae13e17a7604899b785a1b4f
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-02-26 16:31:25 +0530

    integrate watermark

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import authProperty from "./routes/property.js";
 import authAccount from "./routes/account.js";
 import authContact from "./routes/contact.js";
 import authAdmin from "./routes/admin.js";
+import authWatermark from "./routes/watermark.js"
 const app = express();
 app.use(express.static("./public"));
 
-var whitelist = ["https://www.propertyease.in", "https://propertyease.in"];
+var whitelist = ["https://www.propertyease.in", "https://propertyease.in" ];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -26,6 +27,7 @@ app.use("/api/act", authAccount);
 app.use("/api/pro", authProperty);
 app.use("/api/contact", authContact);
 app.use("/api/admin", authAdmin);
+app.use("/api/watermark", authWatermark);
 app.listen(8010, () => {
   console.log("App is running ");
 });
