1d89ff5c65563d3ae24596aaa0ee534be0a476b7
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-03-06 09:51:11 +0530

    email in post req

--- routes/contact.js
+++ routes/contact.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { askquestion, freeEnquiry } from "../controllers/contact.js";
+import { askquestion, freeEnquiry, interestShowed } from "../controllers/contact.js";
 
 const router = express.Router();
 
 router.post("/askquestion", askquestion);
 router.post("/freeEnquiry", freeEnquiry);
+router.post("/interestShowed", interestShowed);
 export default router;
