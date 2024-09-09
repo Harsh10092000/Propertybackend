02c4406da6175e8f868977f337a2180b894b7a8c
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-09-09 10:08:58 +0530

    commit

--- controllers/login.js
+++ controllers/login.js
@@ -85,8 +85,8 @@ export const sendOtp = (req, res) => {
       updateOtp(otp, req.params.email);
       transporter.sendMail(info, (err, data) => {
         if (err) return res.status(500).json(err);
-        //const mobile_number = "917404302678";
-        //sendOtpOnMobile2(mobile_number, otp);
+        const mobile_number = "917404302678";
+        sendOtpOnMobile2(mobile_number, otp);
         return res.status(200).json("Otp Sent");
       });
     } else {
