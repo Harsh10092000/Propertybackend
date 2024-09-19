7d39c145a6e2ac3563a5517c0bc8f16091997e5f
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2024-09-19 17:48:43 +0530

    commit

--- routes/invite.js
+++ routes/invite.js
@@ -1,7 +1,5 @@
 import express from "express";
-import { adminInvite, userInvite, getMailContactList, addSingleMail, deleteMailContact, 
-    //getMailContent 
-} from "../controllers/invite.js";
+import { adminInvite, userInvite, getMailContactList, addSingleMail, deleteMailContact, getMailContent } from "../controllers/invite.js";
 
 const router = express.Router();
 router.post("/adminInvite", adminInvite);
@@ -9,7 +7,7 @@ router.post("/userInvite", userInvite);
 router.get("/getMailContactList", getMailContactList);
 router.post("/addSingleMail", addSingleMail);
 router.delete("/deleteMailContact/:mailId", deleteMailContact);
-//router.get("/getMailContent", getMailContent)
+router.get("/getMailContent", getMailContent)
 
 
 export default router;
